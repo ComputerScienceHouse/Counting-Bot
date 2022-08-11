62b84bbf23053de52f7bcd4c6b2dc38c8479740c
Joe Abbate <josephabbateny@gmail.com>
2022-08-11 00:27:16 -0400

    Add Support for Docker Containerization
    Replaces bot.pems with an Environment variable and allows for
    containerization using Docker.

--- Program.cs
+++ Program.cs
@@ -1,26 +1,17 @@
-﻿//#define WRITE_KEY
-#define RUN_BOT
-
-using CountVonCount;
+﻿using CountVonCount;
 using c = System.Console;
 
 
 
-#if RUN_BOT
-// default config:
+
 Config.Channel = "counting";
 Config.WaitTimeSeconds = 3600;
 Config.OkCountEmoji = "white_check_mark";
 Config.BadCountEmoji = "x";
-    
+
 // TODO: serializable config
 
 Counter.Run().Wait();
-c.WriteLine("Press any key to stop the bot...");
-c.ReadKey();
-#endif
-
-#if WRITE_KEY
-c.WriteLine("Enter a token type, and then the token:");
-Tokens.CreateToken(c.ReadLine()!, c.ReadLine()!);
-#endif
+while (true) {
+    // Stay alive, forever
+}
